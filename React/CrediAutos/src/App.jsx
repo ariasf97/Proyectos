6065aad980c28197ef922c05d81910ee31bdfb39
Harrison arias <ariasfajardoharrison@gmail.com>
2023-11-02 18:28:42 -0500

    ConversionMessage and  ButtonAction

--- React/CrediAutos/src/App.jsx
+++ React/CrediAutos/src/App.jsx
@@ -2,12 +2,14 @@ import './App.css'
 import { HeaderCA } from './components/HeaderCA'
 import { HeroCA } from './components/HeroCA'
 import { FooterCA } from './components/FooterCA'
+import { ConversionMessage } from './components/ConversionMessage'
 
 function App() {
   return (
     <>
       <HeaderCA />
-      <HeroCA />  
+      <HeroCA />
+      <ConversionMessage/>
       <FooterCA/>
     </>
   )
---
+++ React/CrediAutos/src/components/ButtonAction.jsx
@@ -0,0 +1,6 @@
+import '../assets/styles/ButtonAction.css';
+export function ButtonAction() {
+    return (
+        <button className="ca-conversionMessage-button-action">Solicitar mi crédito</button>
+    );
+}
